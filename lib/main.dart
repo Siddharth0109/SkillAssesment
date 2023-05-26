6fed2f2bbc155da63a910a726bb36f5542fa159f
CisGaurav <gaurav.sam@cisinlabs.com>
2023-05-26 18:54:32 +0530

    code updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,29 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Skill Assessment',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Skill Assessment Home'),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +32,224 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.grey,
+        width: double.infinity,
+        height: double.infinity,
+        child: Page(),
+        // child: MainContent(),
+      ),
+    );
   }
+}
+
+class Page extends StatefulWidget {
+  @override
+  PageState createState() => PageState();
+}
+
+class PageState extends State<Page> {
+  double left = 0;
+  double top = 0;
+  late double startHori;
+  late double startVert;
+  double containerWidth = 100;
+  double containerHeight = 100;
+
+  late double greenTop;
+  late double greenLeft;
+
+  late double redTop;
+  late double redLeft;
+
+  late var topVal;
+  late var leftVal;
+
+  late var globalPositionDetail;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    greenTop = MediaQuery.of(context).size.height -
+        MediaQuery.of(context).size.height / 2;
+    greenLeft = MediaQuery.of(context).size.width -
+        MediaQuery.of(context).size.width / 3;
+
+    redTop = MediaQuery.of(context).size.height -
+        MediaQuery.of(context).size.height / 1.2;
+    redLeft = MediaQuery.of(context).size.width -
+        MediaQuery.of(context).size.width / 1.2;
+
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      color: Colors.grey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.5,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.white,
+            child: Stack(
+              children: [
+                GridView.builder(
+                  itemCount: 4,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: index == 0
+                            ? Colors.red
+                            : index == 1
+                                ? Colors.grey
+                                : index == 2
+                                    ? Colors.grey
+                                    : Colors.green,
+                      ),
+                    );
+                  },
+                  padding: const EdgeInsets.all(28),
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 5,
+                      mainAxisSpacing: 5),
+                ),
+                Positioned(
+                    top: top,
+                    left: left,
+                    child: GestureDetector(
+                      onPanStart: (details) {
+                        setState(() {
+                          startHori = details.globalPosition.dx;
+                          startVert = details.globalPosition
+                              .dy; // top = 207.64285714285714  // 415.2857142857143 // 205.71428571428572 // 800.8571428571429
+                        });
+                      },
+
+                      // green top(bottom) => MediaQuery.of(context).size.height / 2.2.toDouble();
+                      // green top (top) => MediaQuery.of(context).size.height / 4.toDouble();
+
+                      // green left (left) => MediaQuery.of(context).size.height / 2.toDouble();
+                      // green left (right) => MediaQuery.of(context).size.height / 1.1.toDouble();
+
+                      onPanDown: (details) {},
+                      onPanEnd: (details) {
+                        setState(() {
+                          /*  top =
+                              MediaQuery.of(context).size.height / 4.toDouble();
+                          left = MediaQuery.of(context).size.width /
+                              1.1.toDouble();*/
+
+// top-bottom = 2.5 = 332.2285714285714
+// top-top = 4.5 = 184.57142857142856
+
+// left-left = 4.5 = 184.57142857142856
+// left-right = 2.5 = 184.57142857142856
+
+
+
+                         /* print("top-top => ${MediaQuery.of(context).size.height /
+                              4.5.toDouble()}");
+                          print("top-bottom => ${MediaQuery.of(context).size.height /
+                              2.5.toDouble()}");
+                          print("left-left => ${MediaQuery.of(context).size.height /
+                              3.toDouble()}");
+                          print("left-right => ${MediaQuery.of(context).size.height /
+                              1.1.toDouble()}");*/
+                          print("topVal => ${topVal}");
+                          print("leftVal => ${leftVal}");
+                          print("top => ${top}");
+                          print("left => ${left}");
+
+
+                          top = MediaQuery.of(context).size.height /
+                              4.5.toDouble();
+
+                          left = MediaQuery.of(context).size.width /
+                              2.5.toDouble();
+
+                          print("top => ${top}");
+                          print("left => ${left}");
+
+                       /*   if ((topVal >
+                                      MediaQuery.of(context).size.height /
+                                          4.toDouble() &&
+                                  topVal <
+                                      MediaQuery.of(context).size.height /
+                                          2.2.toDouble()) &&
+                              leftVal >
+                                  MediaQuery.of(context).size.height /
+                                      3.toDouble() &&
+                              topVal <
+                                  MediaQuery.of(context).size.height /
+                                      1.1.toDouble()) {
+                            top = greenTop;
+                            left = greenLeft;
+                          }else{
+                            top = redTop;
+                            left = redLeft;
+                          }*/
+
+                          /* if (topVal != greenTop) {
+                            top = greenTop;
+                          }
+
+                          if (leftVal != greenLeft) {
+                            left = greenLeft;
+                          }*/
+                        });
+                      },
+                      onPanUpdate: (details) {
+                        var screenWidth = MediaQuery.of(context).size.width;
+                        var screenHeight = MediaQuery.of(context).size.height;
+
+                        globalPositionDetail = details.globalPosition;
+
+                        topVal = details.globalPosition.dy - startVert;
+                        leftVal = details.globalPosition.dx - startHori;
+
+                        var leftBounds = screenWidth - containerWidth;
+                        var topBounds = screenHeight - containerHeight;
+
+                        setState(() {
+                          if (leftVal.floor() >= 0 && leftVal <= leftBounds) {
+                            left = leftVal.floorToDouble();
+                          }
+
+                          if (leftVal < 0) {
+                            left = 0;
+                          }
+
+                          if (leftVal > leftBounds) {
+                            left = leftBounds.floorToDouble();
+                          }
+
+                          if (topVal.floor() >= 0 && topVal <= topBounds) {
+                            top = topVal.floorToDouble();
+                          }
+
+                          if (topVal < 0) {
+                            top = 0;
+                          }
+
+                          if (topVal > topBounds) {
+                            top = topBounds.floorToDouble();
+                          }
+                        });
+                      },
+                      child: const Icon(Icons.star,
+                          color: Colors.yellow, size: 50),
+                    )),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
